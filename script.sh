3aef4023c043b98343ed8da349c70fe8b4f75047
garyluu <garylau.work@gmail.com>
2019-11-05 12:47:59 -0500

    Prettify swagger.yaml

--- script.sh
+++ script.sh
@@ -2,7 +2,8 @@
 set -x
 
 git checkout -f master
-curl -o swagger.yaml 'https://quay.io//api/v1/discovery?' -H 'x-requested-with: XMLHttpRequest'
+curl -o rawswagger.yaml 'https://quay.io//api/v1/discovery?' -H 'x-requested-with: XMLHttpRequest'
+jq . rawswagger.yaml > swagger.yaml
 git diff --exit-code
 if [ $? == 0 ] ; then
   exit 1
